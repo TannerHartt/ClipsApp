c7749d8074e9ffb549a3edde008743a4cee43bb2
Tanner Hart <tannerhart24@yahoo.com>
2022-06-28 15:55:12 -0700

    Created tabs functionality to login/register form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from "@angular/router";
+import { UserModule } from "./user/user.module";
+import { NavComponent } from './nav/nav.component';
+
+const routes: Routes = [
+
+  // { path: '**', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
