02f8064829cf204cdd1daea663c7cb5b9c593777
Tanner Hart <tannerhart24@yahoo.com>
2022-06-29 12:59:52 -0700

    Filled out the alert component to display an alert at the top of an invalid form

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertComponent implements OnInit {
 
+  @Input() color: string = 'blue';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get bgColor() {
+    return `bg-${this.color}-400`;
+  }
 }
