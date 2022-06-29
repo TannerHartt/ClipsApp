dde6e50116a7225025a2a1901030009207f49325
Tanner Hart <tannerhart24@yahoo.com>
2022-06-29 11:55:01 -0700

    Added validators to each form control in my reactive form.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -9,10 +9,10 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 export class RegisterComponent {
   name = new FormControl('', [Validators.minLength(4), Validators.required]);
   email = new FormControl('', [Validators.email, Validators.required]);
-  age = new FormControl('',[Validators.required]);
-  password = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  age = new FormControl('',[Validators.required, Validators.min(18), Validators.max(120)]);
+  password = new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]);
   confirm_password = new FormControl('', [Validators.required]);
-  number = new FormControl('',[Validators.required]);
+  number = new FormControl('',[Validators.required, Validators.minLength(13), Validators.maxLength(13)]);
 
 
   registerForm = new FormGroup({
@@ -23,5 +23,9 @@ export class RegisterComponent {
     confirm_password: this.confirm_password,
     number: this.number
   });
+
+  register() {
+
+  }
 
 }
