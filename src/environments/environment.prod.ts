ffd82d5875149eafc2acd1246f274d8767c24f52
Tanner Hart <tannerhart24@yahoo.com>
2022-07-19 15:47:21 -0700

    updated environment files to include the firebase environment to the whole project.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCBspjHETz4S13-W3t683qivyb9U0SKEzo",
+    authDomain: "clips-24295.firebaseapp.com",
+    projectId: "clips-24295",
+    storageBucket: "clips-24295.appspot.com",
+    appId: "1:51118724949:web:f1c69f517e64951dd181dc",
+    measurementId: "G-PP7XYPV9Z2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCBspjHETz4S13-W3t683qivyb9U0SKEzo",
+    authDomain: "clips-24295.firebaseapp.com",
+    projectId: "clips-24295",
+    storageBucket: "clips-24295.appspot.com",
+    appId: "1:51118724949:web:f1c69f517e64951dd181dc",
+    measurementId: "G-PP7XYPV9Z2"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
