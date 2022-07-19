9b8e5280c6959e945aabf099174fceb5e31857a7
Tanner Hart <tannerhart24@yahoo.com>
2022-07-19 16:04:57 -0700

    Updated app module with several necessary imports and additional routes for all current components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { VideoModule } from './video/video.module';
 
+import { environment  } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes} from "@angular/router";
-import { UserModule } from "./user/user.module";
+import { RouterModule, Routes} from '@angular/router';
+import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ClipComponent } from './clip/clip.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'clip/:id', component: ClipComponent },
 
-  // { path: '**', component: AppComponent}
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
+    AboutComponent,
+    ClipComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    VideoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
