4aaf59bcb424bc349a16c1f8d289980397764c3a
Tanner Hart <tannerhart24@yahoo.com>
2022-07-19 16:21:14 -0700

    Created separate module used to control all components and routes related to video upload and management.

---
+++ src/app/video/video-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
+
+const routes: Routes = [
+  {
+    path: 'manage',
+    component: ManageComponent,
+    data: {
+      authOnly: true
+    }
+  },
+  { path: 'upload', component: UploadComponent, data: { authOnly: true } },
+  { path: 'manage-clips', redirectTo: 'manage' },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class VideoRoutingModule { }
