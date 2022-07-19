7f825bfe980f84382237834cb6ea0f70f3e20bd5
Tanner Hart <tannerhart24@yahoo.com>
2022-07-19 16:03:48 -0700

    Made a function to control the login functionality as well as showing the correct alert message on the attempt. Auto login for revisiting users.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,33 @@ export class LoginComponent implements OnInit {
     email: '',
     password: '',
   }
+  showAlert = false;
+  alertMsg = 'Please wait, we are logging you in.'
+  alertColor = 'blue';
+  inSubmission = false;
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
 
-  login() {
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait, we are logging you in.'
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+    try {
+      await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password);
+    } catch(e) {
+        this.inSubmission = false;
+        this.alertMsg = 'An unexpected error occurred, please try again later';
+        this.alertColor = 'red';
 
+        return
+    }
+
+    this.alertMsg = 'Success! You are logged in';
+    this.alertColor = 'green';
   }
 
 }
