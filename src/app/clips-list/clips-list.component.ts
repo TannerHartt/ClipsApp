63dfa59c37db37e20761839dc6f73dd82b55c2dd
Tanner Hart <tannerhart24@yahoo.com>
2023-03-20 13:54:23 -0700

    Added garbage collection and made component more reusuable by preventing scrolling if specified

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { ClipService } from '../services/clip.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clips-list',
-  templateUrl: './clips-list.component.html'  
+  templateUrl: './clips-list.component.html',
+  providers: [DatePipe]
 })
 export class ClipsListComponent implements OnInit {
+  @Input() scrollable: boolean = true;
 
   constructor(public clipService: ClipService) { 
     this.clipService.getClips();
   }
 
   ngOnInit(): void {
-    window.addEventListener('scroll', this.handleScroll);
+    if (this.scrollable) {
+      window.addEventListener('scroll', this.handleScroll);
+    }
   }
 
   ngOnDestroy(): void {
-    window.removeEventListener('scroll', this.handleScroll);
+    if (this.scrollable) {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    this.clipService.pageClips = [];
   }
 
   handleScroll = () => {
