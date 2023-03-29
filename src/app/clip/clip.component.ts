8430920fc082d9081b1b5dcbd464a0028edcee88
Tanner Hart <tannerhart24@yahoo.com>
2023-03-29 14:23:52 -0700

    Refactored to fetch and store an individual clip dynamically from firebase.

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
+import { iClip } from '../models/clip.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.css']
+  styleUrls: ['./clip.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe]
 })
 export class ClipComponent implements OnInit {
-  id: string = '';
   @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
   player?: videojs.Player;
+  clip?: iClip;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.player = videojs(this.target?.nativeElement);
 
-    this.route.params.subscribe((params: Params) => {
-      this.id = params.id; // Provides the clip ID in the route parameters.
+    this.route.data.subscribe(data => {
+      this.clip = data.clip as iClip;
+
+      this.player?.src({
+        src: this.clip.url,
+        type: 'video/mp4'
+      });
     });
   }
 
